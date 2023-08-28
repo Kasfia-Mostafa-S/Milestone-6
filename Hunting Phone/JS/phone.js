30c9123d708f4b8d70072b2fdbd6af47675c8724
SwornaCode <kasfiamostafa03@gmail.com>
2023-08-28 15:48:34 +0600

    show details

--- Hunting Phone/JS/phone.js
+++ Hunting Phone/JS/phone.js
@@ -39,8 +39,8 @@ const displayPhone = (phones,isShowAll) => {
     <div class="card-body">
       <h2 class="card-title">${phone.phone_name}</h2>
       <p>If a dog chews shoes whose shoes does he choose?</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+      <div class="card-actions justify-center">
+        <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
       </div>
     </div>
     `;
@@ -50,6 +50,14 @@ const displayPhone = (phones,isShowAll) => {
 
   //  hide spinner
   toggleLoadingSpinner(false)
+}
+
+const handleShowDetails = async (id) => {
+  console.log('clicked show details',id)
+  // loadData
+  const res = await fetch(` https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json();
+  console.log(data)
 }
 
 // handle search button
